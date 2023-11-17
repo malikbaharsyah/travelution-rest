41f18c52656be5d13cc499f50fd1ed9f801c9e75
Malik Baharsyah <13521029@mahasiswa.itb.ac.id>
2023-11-17 08:05:40 +0700

    feat: js

--- src/main.js
+++ src/main.js
@@ -10,10 +10,12 @@ const auth = require('./controllers/auth');
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 
